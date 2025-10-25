b9412563e2807d77318d879192aa6878ee68524d
Ryugi62 <xorjf1027@naver.com>
2025-10-25 13:03:01 +0900

    헤더 컴포넌트 레이아웃 수정: 내부 div 추가 및 스타일 조정

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -9,27 +9,27 @@ export default function HeaderComponent() {
     ]
 
     return (
-        <div className="h-full min-h-[77px] flex items-center">
-            <Image
-                src={LogoImage}
-                alt="Logo"
-                className="object-contain h-[32px] max-w-[215px]"
-            />
+        <div className="max-h-[77px] h-full border-b border-[#ddd]">
+            <div className="max-w-[1280px] h-full flex items-center m-auto">
+                <Image
+                    src={LogoImage}
+                    alt="Logo"
+                    className="object-contain h-[32px]"
+                />
 
-            <nav
-                className="h-full"
-            >
-                {
-                    navArr.map((element, idx) => {
-                        const [[label, href]] = Object.entries(element);
-                        return (
-                            <a key={idx} href={href} className="mx-2">
-                                {label}
-                            </a>
-                        );
-                    })
-                }
-            </nav>
+                <nav className="h-full ml-auto">
+                    {
+                        navArr.map((element, idx) => {
+                            const [[label, href]] = Object.entries(element);
+                            return (
+                                <a key={idx} href={href} className="mx-2">
+                                    {label}
+                                </a>
+                            );
+                        })
+                    }
+                </nav>
+            </div>
         </div>
     )
 }
